5a30f0dd680f5c7f3538f6c52f90ff4ad8ead9eb
hcwinsemius <h.c.winsemius@gmail.com>
2024-10-03 13:42:10 +0200

    numpy compatible funcs and multi-image and tests #4

--- pivnumba/jitfuncs.py
+++ pivnumba/jitfuncs.py
@@ -6,31 +6,50 @@ import numba as nb
 
 @nb.njit(cache=True)
 def rfft2(x):
+    """JIT variant of rfft2."""
     return np.fft.rfft2(x)
 
 
 @nb.njit(cache=True)
 def rfft2_x(x, fsize):
+    """JIT variant of rfft2 with fsize parameter."""
     return np.fft.rfft2(x, fsize)
 
 
 @nb.njit(cache=True)
 def irfft2(x):
+    """JIT variant of irfft2."""
     return np.fft.irfft2(x)
 
 
 @nb.njit(cache=True)
 def fftshift(x, axes):
+    """JIT variant of fftshift."""
     return np.fft.fftshift(x, axes)
 
 
 @nb.njit(cache=True)
 def conj(x):
+    """JIT variant of conj"""
     return np.conj(x)
 
 
 @nb.njit(nb.float64[:,:](nb.uint8[:, :]), cache=True)
-def normalize_intensity(img):
+def normalize_intensity(
+        img: nb.uint8
+) -> nb.float64:
+
+    """
+    Normalize intensity of an image interrogation window.
+
+    Parameters
+    ----------
+    img : np.ndarray (w * y * x)
+
+    Returns
+    -------
+
+    """
     img_mean = np.mean(img)
     img = img - img_mean
     img_std = np.std(img)
@@ -45,13 +64,19 @@ def normalize_intensity(img):
 
 # TODO: remove once benchmarking is performed
 def normalize_intensity_numpy(img):
-    img_mean = np.mean(img)
+    img_mean = img.mean(axis=(-2, -1), keepdims=True)
     img = img - img_mean
-    img_std = np.std(img)
-    if img_std != 0:
-        img = img/img_std
-    else:
-        img = np.zeros_like(img)
+    img_std = img.std(axis=(-2, -1), keepdims=True)
+    img = np.divide(
+        img,
+        img_std,
+        out=np.zeros_like(img),
+        where=(img_std != 0)
+    )
+    # if img_std != 0:
+    #     img = img/img_std
+    # else:
+    #     img = np.zeros_like(img)
     # if imb_std != 0:
     #     imb = imb/ima_std
     return img
@@ -82,85 +107,94 @@ def ncc(image_a, image_b):
         res[n] = fftshift(irfft2(f2a * f2b).real, axes=(-2, -1))
     return res
 
-# TODO: remove once benchmarking is performed
+
 def ncc_numpy(image_a, image_b):
-    # image_a = normalize_intensity_numpy(image_a)
-    # image_b = normalize_intensity_numpy(image_b)
+    image_a = normalize_intensity_numpy(image_a)
+    image_b = normalize_intensity_numpy(image_b)
     f2a = np.conj(np.fft.rfft2(image_a))
     f2b = np.fft.rfft2(image_b)
     return np.fft.fftshift(np.fft.irfft2(f2a * f2b).real, axes=(-2, -1))
 
 
-@nb.njit(cache=True)
-def peak_position(corr, subpixel_method="gaussian"):
-    # default_peak_position = np.array([0,0])
-    eps = 1e-7
-    # subp_peak_position = tuple(np.floor(np.array(corr.shape)/2))
-    subp_peak_position = (0., 0.)  # any wrong position will mark nan
+@nb.njit(nb.float64[:, :, :, :](nb.uint8[:, :, :, :]), cache=True, parallel=True, nogil=True)
+def multi_img_ncc(imgs):
+    """Compute correlation over all image pairs in `imgs`."""
+    # the output corr array is one stride smaller than the input imgs array, because it uses frame pairs
+    corr = np.empty((len(imgs) - 1, imgs.shape[-3], imgs.shape[-2], imgs.shape[-1]), dtype=nb.float64)
+    for n in nb.prange(len(imgs) - 1):
+        res = ncc(imgs[n], imgs[n + 1])
+        corr[n] = res
+    return corr
 
-    # get row/col of peak location
-    #
+
+def multi_img_ncc_numpy(imgs):
+    # the output corr array is one stride smaller than the input imgs array, because it uses frame pairs
+    corr = np.empty((len(imgs) - 1, imgs.shape[-3], imgs.shape[-2], imgs.shape[-1]))
+    for n in range(len(imgs) - 1):
+        res = ncc_numpy(imgs[n], imgs[n + 1])
+        corr[n] = res
+    return corr
+
+
+@nb.njit(cache=True)
+def peak_position(corr):
+    eps = 1e-7
     idx = np.argmax(corr)
     peak1_i, peak1_j = idx // len(corr), idx % len(corr)
-
-    # peak1_i, peak1_j = np.unravel_index(np.argmax(corr), corr.shape)
-
-    # the peak and its neighbours: left, right, down, up
-    # but we have to make sure that peak is not at the border
-    # @ErichZimmer noticed this bug for the small windows
-    #
-    # if ((peak1_i == 0) | (peak1_i == corr.shape[0]-1) |
-    #    (peak1_j == 0) | (peak1_j == corr.shape[1]-1)):
-    #     return subp_peak_position
-    # else:
     corr = corr + eps  # prevents log(0) = nan if "gaussian" is used (notebook)
     c = corr[peak1_i, peak1_j] + eps
     cl = corr[peak1_i - 1, peak1_j] + eps
     cr = corr[peak1_i + 1, peak1_j] + eps
     cd = corr[peak1_i, peak1_j - 1] + eps
     cu = corr[peak1_i, peak1_j + 1] + eps
-        #
-        # # gaussian fit
-        # if np.logical_and(np.any(np.array([c, cl, cr, cd, cu]) < 0),
-        #                   subpixel_method == "gaussian"):
-        #     subpixel_method = "parabolic"
-        #
-        # # try:
-        # if subpixel_method == "centroid":
-        #     subp_peak_position = (
-        #         ((peak1_i - 1) * cl + peak1_i * c + (peak1_i + 1) * cr) /
-        #         (cl + c + cr),
-        #         ((peak1_j - 1) * cd + peak1_j * c + (peak1_j + 1) * cu) /
-        #         (cd + c + cu),
-        #     )
-        #
-        # elif subpixel_method == "gaussian":
+
+    # gaussian peak
     nom1 = np.log(cl) - np.log(cr)
     den1 = 2 * np.log(cl) - 4 * np.log(c) + 2 * np.log(cr) + eps
     nom2 = np.log(cd) - np.log(cu)
     den2 = 2 * np.log(cd) - 4 * np.log(c) + 2 * np.log(cu) + eps
 
     subp_peak_position = np.array([peak1_i + nom1/den1, peak1_j + nom2/den2])
-        # peak1_i + np.divide(nom1, den1, out=np.zeros(1),
-        #                     where=(den1 != 0.0))[0],
-        # peak1_j + np.divide(nom2, den2, out=np.zeros(1),
-        #                     where=(den2 != 0.0))[0],
-    #
-    # elif subpixel_method == "parabolic":
-    #     subp_peak_position = (
-    #         peak1_i + (cl - cr) / (2 * cl - 4 * c + 2 * cr),
-    #         peak1_j + (cd - cu) / (2 * cd - 4 * c + 2 * cu),
-    #     )
-    #
     return subp_peak_position
 
+
+def peak_position_numpy(corr):
+    eps = 1e-7
+
+    # Find argmax along axis (1, 2) for each 2D slice of the input
+    idx = np.argmax(corr.reshape(corr.shape[0], -1), axis=1)
+    peak1_i = idx // corr.shape[1]
+    peak1_j = idx % corr.shape[2]
+
+    # Adding eps to avoid log(0)
+    corr = corr + eps
+
+    # Indexing the peak and neighboring points for vectorized operations
+    c = corr[np.arange(corr.shape[0]), peak1_i, peak1_j] + eps
+    cl = corr[np.arange(corr.shape[0]), peak1_i - 1, peak1_j] + eps
+    cr = corr[np.arange(corr.shape[0]), peak1_i + 1, peak1_j] + eps
+    cd = corr[np.arange(corr.shape[0]), peak1_i, peak1_j - 1] + eps
+    cu = corr[np.arange(corr.shape[0]), peak1_i, peak1_j + 1] + eps
+
+    # Gaussian peak calculations (nom1, den1, nom2, den2)
+    nom1 = np.log(cl) - np.log(cr)
+    den1 = 2 * np.log(cl) - 4 * np.log(c) + 2 * np.log(cr) + eps
+    nom2 = np.log(cd) - np.log(cu)
+    den2 = 2 * np.log(cd) - 4 * np.log(c) + 2 * np.log(cu) + eps
+
+    # Subpixel peak position
+    subp_peak_position = np.vstack([
+        peak1_i + nom1 / den1,
+        peak1_j + nom2 / den2
+    ]).T
+
+    return subp_peak_position
 
 @nb.njit(parallel=True, cache=True)
 def u_v_displacement(
         corr,
         n_rows,
         n_cols,
-        # subpixel_method="gaussian"
 ):
     """
     Correlation maps are converted to displacement for each interrogation
@@ -179,7 +213,9 @@ def u_v_displacement(
 
     # center point of the correlation map
     default_peak_position = np.floor(
-        np.array(corr[0, :, :].shape)/2
+        np.array(
+            corr[0, :, :].shape
+        ) / 2
     )
     for k in nb.prange(n_rows):
         for m in nb.prange(n_cols):
@@ -187,15 +223,20 @@ def u_v_displacement(
             # the find_subpixel_peak_position returns
             peak = peak_position(
                 corr[k * n_cols + m],
-                subpixel_method="gaussian"
             ) - default_peak_position
-               # type: ignore
-
-            # the horizontal shift from left to right is the u
-            # the vertical displacement from top to bottom (increasing row) is v
-            # x the vertical shift from top to bottom is row-wise shift is now
-            # a negative vertical
             u[k, m] = peak[1]
             v[k, m] = peak[0]
+
+    return u, v
+
+def u_v_displacement_numpy(corr, n_rows, n_cols):
+    peaks = peak_position_numpy(corr)
+    peaks_def = np.floor(
+        np.array(
+            corr[0, :, :].shape
+        ) / 2
+    )
+    u = peaks[:, 1].reshape(n_rows, n_cols) - peaks_def[1]
+    v = peaks[:, 0].reshape(n_rows, n_cols) - peaks_def[0]
 
     return u, v
--- tests/test_fft.py
+++ tests/test_fft.py
@@ -3,14 +3,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 
+from numpy.ma.testutils import assert_equal
+from openpiv.pyprocess import vectorized_correlation_to_displacements
 from pytest import fixture
 
 from pivnumba import jitfuncs, window
-from openpiv.pyprocess import vectorized_correlation_to_displacements
 
 
 @fixture
-def img_pair(imgs):
+def imgs_win(imgs):
     # get the x and y coordinates per window
     win_x, win_y = window.sliding_window_idx(imgs[0])
     # apply the coordinates on all images
@@ -20,8 +21,13 @@ def img_pair(imgs):
         win_y,
         swap_time_dim=False
     )
+    return window_stack
+
+
+@fixture
+def img_pair(imgs_win):
     # only return image 0 and 1
-    img_pair = window_stack[0:2]
+    img_pair = imgs_win[0:2]
     return img_pair
 
 
@@ -39,44 +45,42 @@ def dims(imgs):
 @fixture
 def correlations(img_pair):
     corrs = jitfuncs.ncc(*img_pair)
-    return corrs*np.random.rand(*corrs.shape)*0.03
+    return corrs*np.random.rand(*corrs.shape)*0.005
 
 
 def test_ncc(img_pair):
     """Test correlation analysis on a pair of image windows."""
     image_a, image_b = img_pair
     t1 = time.time()
-    res = jitfuncs.ncc(image_a, image_b)
-    t2 = time.time()
-    print(f"\nAny required compilation took {t2-t1} secs.")
-
-    t1 = time.time()
-    for n in nb.prange(200):
-        # res = ncc_numba_at_once(np.stack([image_a for m in range(n +1)]), np.stack([image_b for m in range(n+1)]))
-        image_a_ = nb.uint8(nb.float32(image_a) * np.random.rand(*image_a.shape))
-        image_b_ = nb.uint8(nb.float32(image_b) * np.random.rand(*image_b.shape))
-        res = jitfuncs.ncc(image_a_, image_b_)
-        if n == 0:
-            print(f"First iteration took {time.time() - t1} seconds")
-        # res2 = np.array([ncc_numba(a, b) for a,b in zip(image_a, image_b)])
+    res_nb = jitfuncs.ncc(image_a, image_b)
     t2 = time.time()
     time_nb = t2-t1
     print(f"Numba took {time_nb} secs.")
-
-
     t1 = time.time()
-    for n in range(200):
-        image_a_ = np.uint(np.float32(image_a) * np.random.rand(*image_a.shape))
-        image_b_ = np.uint(np.float32(image_b) * np.random.rand(*image_b.shape))
+    res_np = jitfuncs.ncc_numpy(image_a, image_b)
+    t2 = time.time()
+    time_np = t2-t1
+    print(f"Numpy took {time_np} secs.")
+    assert np.allclose(res_nb, res_np)
+    # TODO: also test if values are close to expected values
 
-        # res = ncc_numba_at_once(np.stack([image_a for m in range(n +1)]), np.stack([image_b for m in range(n+1)]))
-        res = jitfuncs.ncc_numpy(image_a_, image_b_)
-        if n == 0:
-            print(f"First iteration took {time.time() - t1} seconds")
-        # res2 = np.array([ncc_numba(a, b) for a,b in zip(image_a, image_b)])
+
+def test_multi_img_ncc(imgs_win):
+    """Test cross correlation with several hundreds of images."""
+    imgs_win_ = np.uint8(imgs_win)
+    for n in range(10):
+        imgs_win = np.concatenate([imgs_win, np.uint8(np.float32(imgs_win_) * np.random.rand(*imgs_win_.shape))], axis=0)
+    t1 = time.time()
+    res_nb = jitfuncs.multi_img_ncc(imgs_win)
+    t2 = time.time()
+    time_nb = t2-t1
+    print(f"Numba took {time_nb} secs.")
+    t1 = time.time()
+    res_np = jitfuncs.multi_img_ncc_numpy(imgs_win)
     t2 = time.time()
     time_nb = t2-t1
     print(f"Numpy took {time_nb} secs.")
+    assert np.allclose(res_nb, res_np)
 
 
 def test_u_v_displacement(correlations, dims):
@@ -90,10 +94,14 @@ def test_u_v_displacement(correlations, dims):
 
     t1 = time.time()
     for n in range(2000):
-        u2, v2 = vectorized_correlation_to_displacements(correlations, n_rows, n_cols, subpixel_method="gaussian")
+        u2, v2 = jitfuncs.u_v_displacement_numpy(correlations, n_rows, n_cols)  # vectorized_correlation_to_displacements(correlations, n_rows, n_cols, subpixel_method="gaussian")
     t2 = time.time()
     print(f"Peak position search with OpenPIV took {t2 - t1} seconds")
 
     plt.quiver(u2, v2, color="r", alpha=0.5)
     plt.quiver(u, v, color="b", alpha=0.5)
     plt.show()
+
+def test_peaks_numpy(correlations):
+    peaks = jitfuncs.peak_position_numpy(correlations)
+    print(peaks)
