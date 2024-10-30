f2a6f0a0af2ed61ed36d50653cee371dd58a26c2
hcwinsemius <h.c.winsemius@gmail.com>
2024-10-30 10:16:08 +0100

    fix #11

---
+++ ffpiv/__init__.py
@@ -0,0 +1,6 @@
+"""ffpiv: Fast and flexible Particle Image Velocimetry analysis powered by numba."""
+
+__version__ = "0.1.0.dev"
+
+from . import pnb, pnp, window
+from .api import *
--- pivnumba/__init__.py
+++
@@ -1,6 +0,0 @@
-"""openpiv-numba: Numba accelerated Particle Image Velocimetry analysis."""
-
-__version__ = "0.1.0.dev"
-
-from . import nb, window
-from .api import *
