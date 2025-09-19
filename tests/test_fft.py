1f93a9607c8f6f00f3719de509fbb0209b53fe64
hcwinsemius <h.c.winsemius@gmail.com>
2025-09-19 17:06:14 +0200

    fix tests #25

--- tests/test_fft.py
+++ tests/test_fft.py
@@ -28,7 +28,7 @@ def dims(imgs):
 
 @pytest.fixture()
 def correlations(img_pair):
-    corrs = pnb.ncc(*img_pair)
+    corrs = pnb.ncc(*img_pair, clip_norm=False)
     return corrs * np.random.rand(*corrs.shape) * 0.005
 
 
@@ -47,22 +47,18 @@ def test_ncc(img_pair, clip_norm):
     time_np = t2 - t1
     print(f"Numpy took {time_np} secs.")
     assert np.allclose(res_nb, res_np, atol=1e-6, rtol=1e-5)
-    plt.imshow(res_nb[0])
-    plt.colorbar()
-    plt.show()
-    # TODO: also test if values are close to expected values
 
 
 def test_multi_img_ncc(imgs_win_stack, mask):
     """Test cross correlation with several hundreds of images."""
     t1 = time.time()
     idx = np.repeat(True, imgs_win_stack.shape[-3])
-    res_nb = pnb.multi_img_ncc(imgs_win_stack, mask, idx)
+    res_nb = pnb.multi_img_ncc(imgs_win_stack, mask, idx, clip_norm=False)
     t2 = time.time()
     time_nb = t2 - t1
     print(f"Numba took {time_nb} secs.")
     t1 = time.time()
-    res_np = pnp.multi_img_ncc(imgs_win_stack, mask)
+    res_np = pnp.multi_img_ncc(imgs_win_stack, mask, clip_norm=False)
     t2 = time.time()
     time_nb = t2 - t1
     print(f"Numpy took {time_nb} secs.")
